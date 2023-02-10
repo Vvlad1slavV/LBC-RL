dce7c5fd0cd47116cbf1800572a289b1b8d2af75
Vlad1slav <vvlad1slavv@yandex.ru>
2023-02-10 13:41:59 +0300

    Fix bug in CarRacingGroundTruthObsWrapper

--- CarRacing/utils.py
+++ CarRacing/utils.py
@@ -31,28 +31,10 @@ class CarRacingGroundTruthObsWrapper(gym.ObservationWrapper):
 
         track = np.array(self.track)
         track_size = track.shape[0]
-        # if track_size > 0:
+
         rot = np.array([[np.cos(car_angle), -np.sin(car_angle)], [np.sin(car_angle), np.cos(car_angle)]])
         arg = np.argmin(distance.cdist([car_position], track[:, 2:4]))
-        print(f'track shape {track.shape}, track_size {track_size}, arg {arg}')
-        print(f'global path {(track[arg+1-track_size:arg+1+self.num_obs_points-track_size:1, 2:4] - car_position).T}')
-        path = rot @ (track[arg+1-track_size:arg+7-track_size:1, 2:4] - car_position).T
+        path = rot @ (track[np.r_[arg+1-track_size:arg+1+self.num_obs_points-track_size], 2:4] - car_position).T
         path_angles = np.expand_dims(np.arctan2(*path), axis=0)
         
-        # print(f'Track shape {track.shape}')
-        # else:
-        #     path = [[0, 0, 0, 0], # x
-        #              1, 2, 3, 4]  # y
-        #     path_angles = [0, 0, 0, 0]
-        # print(path.shape, path_angles.shape)
-        # print(np.concatenate((path.T, path_angles.T), axis=1))
-        # print()
-        # print(path_angles)
-        observ = np.array([velocity, angular_velocity, *np.concatenate((path, path_angles), axis=0).ravel(order='F')])
-        # if observ.shape != self.observation_space.shape:
-        #     path = [[0, 0, 0, 0], # x
-        #              1, 2, 3, 4]  # y
-        #     path_angles = [0, 0, 0, 0]
-        #     observ = np.array([velocity, angular_velocity, *np.concatenate((path, path_angles), axis=0).ravel(order='F')])
-        # print(f'Obs {observ}, arg {arg}, rot {rot}, car pos {car_position}')
-        return observ
+        return np.array([velocity, angular_velocity, *np.concatenate((path, path_angles), axis=0).ravel(order='F')])
