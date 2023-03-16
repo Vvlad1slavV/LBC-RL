d1121c22e2d6b096730e4c14dc04f91d2164b2ac
Vlad1slav <Vvlad1slavV@yandex.ru>
2023-03-16 19:26:03 +0300

    Add choice of ObsWrapper

--- CarRacing/CarRacing_utils.py
+++ CarRacing/CarRacing_utils.py
@@ -8,6 +8,44 @@ class CarRacingGroundTruthObsWrapper(gym.ObservationWrapper):
         super().__init__(env)
         self.num_obs_points = num_obs_points
         self.low_state = np.array(
+            [0, -np.pi, *[0, -np.pi]*self.num_obs_points], dtype=np.float32
+        )
+        self.high_state = np.array(
+            [100, np.pi, *[float('inf'), np.pi]*self.num_obs_points], dtype=np.float32
+        )
+        self.observation_space = gym.spaces.Box(
+            low=self.low_state, high=self.high_state, dtype=np.float32
+        )
+        
+    def observation(self, obs):
+        velocity = np.sqrt(
+            np.square(self.car.hull.linearVelocity[0])
+            + np.square(self.car.hull.linearVelocity[1])
+        )
+        angular_velocity = self.car.hull.angularVelocity
+        # ABS sensors
+        # abs_sensors = []
+        # for i in range(4):
+            # abs_sensors.append(0.01 * self.car.wheels[i].omega)
+        car_position =  np.array(self.car.hull.position)
+        car_angle = -self.car.hull.angle
+
+        track = np.array(self.track)
+        track_size = track.shape[0]
+
+        rot = np.array([[np.cos(car_angle), -np.sin(car_angle)], [np.sin(car_angle), np.cos(car_angle)]])
+        arg = np.argmin(distance.cdist([car_position], track[:, 2:4]))
+        pathxy = rot @ (track[np.r_[arg+1-track_size:arg+1+self.num_obs_points-track_size], 2:4] - car_position).T
+        path_angles = np.expand_dims(np.arctan2(*pathxy), axis=0)
+        path = np.expand_dims(np.linalg.norm(pathxy, axis=0), axis=0)
+        
+        return np.array([velocity, angular_velocity, *np.concatenate((path, path_angles), axis=0).ravel(order='F')])
+
+class CarRacingGroundTruthXYObsWrapper(gym.ObservationWrapper):
+    def __init__(self, env, num_obs_points: int = 6):
+        super().__init__(env)
+        self.num_obs_points = num_obs_points
+        self.low_state = np.array(
             [0, -np.pi, *[-float('inf'), -float('inf'), -np.pi]*self.num_obs_points], dtype=np.float32
         )
         self.high_state = np.array(
