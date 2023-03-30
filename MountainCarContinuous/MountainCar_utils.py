3de6e3e2ce96b4cadf150b5bd9263722f61c6c8f
Vlad1slav <Vvlad1slavV@yandex.ru>
2023-03-30 13:23:09 +0300

    Add NoVel wrapper

--- MountainCarContinuous/MountainCar_utils.py
+++ MountainCarContinuous/MountainCar_utils.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import sys
 
 class MountainCarContinuousObsWrapper(gym.ObservationWrapper):
     def __init__(self, env):
@@ -15,3 +16,15 @@ class MountainCarContinuousObsWrapper(gym.ObservationWrapper):
         
     def observation(self, obs):
         return np.append(obs, np.cos(3 * obs[0]))
+    
+class MountainCarContinuousNoVelObsWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.low_state = self.low_state[0]
+        self.high_state = self.high_state[0]
+        self.observation_space = gym.spaces.Box(
+            low=self.low_state, high=self.high_state, shape=(1,), dtype=np.float32
+        )
+        
+    def observation(self, obs):
+        return np.array(obs[0], ndmin=1)
