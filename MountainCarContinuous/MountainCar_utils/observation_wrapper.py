1933a6b48e55b543f4c6436663bf336094c69ecf
Vlad1slav <vvlad1slavv@yandex.ru>
2023-04-15 12:23:10 +0300

    Refactoring MCC utils

--- MountainCarContinuous/MountainCar_utils/observation_wrapper.py
+++ MountainCarContinuous/MountainCar_utils/observation_wrapper.py
@@ -6,14 +6,14 @@ class MountainCarContinuousObsWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.max_angle = 1
-        
+
         self.low_state = np.append(self.low_state, -self.max_angle)
         self.high_state = np.append(self.high_state, self.max_angle)
-        
+
         self.observation_space = gym.spaces.Box(
             low=self.low_state, high=self.high_state, dtype=np.float32
         )
-        
+
     def observation(self, obs):
         return np.append(obs, np.cos(3 * obs[0]))
     
@@ -25,6 +25,7 @@ class MountainCarContinuousNoVelObsWrapper(gym.ObservationWrapper):
         self.observation_space = gym.spaces.Box(
             low=self.low_state, high=self.high_state, shape=(1,), dtype=np.float32
         )
-        
+
     def observation(self, obs):
         return np.array(obs[0], ndmin=1)
+    
--- MountainCarContinuous/train_PPO_MountainCarContinuous.py
+++ MountainCarContinuous/train_PPO_MountainCarContinuous.py
@@ -1,123 +1,59 @@
-import os
-from typing import Callable
-from collections import deque
 import argparse
 
-import numpy as np
-import gym
-
 from stable_baselines3 import PPO
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecFrameStack
+from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.results_plotter import load_results, ts2xy
+from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
 
-import gnwrapper
+from MountainCar_utils.observation_wrapper import MountainCarContinuousNoVelObsWrapper
+from MountainCar_utils.callbacks import SaveOnBestTrainingRewardCallback
 
-from MountainCar_utils import MountainCarContinuousNoVelObsWrapper
-
-env_id = "MountainCarContinuous-v0"
+ENV_ID = "MountainCarContinuous-v0"
 
 def wrapper(env):
-    env = MountainCarContinuousNoVelObsWrapper(env) 
+    env = MountainCarContinuousNoVelObsWrapper(env)
     return env
 
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """
-    Callback for check reward
-
-    :param check_freq: (int)
-    :param cpu_num: (int) Size of VecEnv
-    :param log_dir: (str) Path to a folder where the evaluations (`.npz`)
-        will be saved. It will be updated at each step.
-    :param model_name: (str)
-    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
-    """
-    def __init__(self, check_freq: int, num_cpu: int, log_path: str, model_name: str, verbose: int = 1):
-        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.num_cpu = num_cpu
-        self.log_path = log_path
-        self.model_name = model_name
-        self.mean_reward = -np.inf
-        self.rew_results = []
-        self.mean_reward_ncpu = []
-        self.rew_timesteps_ncpu = []
-        self.rew_timesteps = []
-        self.rew_length = [] # get from ep_info_buffer
-    
-        self.ep_len = 0
-        self.mean_ep_num = 0
-
-    def _init_callback(self):
-        if self.log_path is not None:
-            os.makedirs(os.path.dirname(self.log_path), exist_ok=True)
-        if self.model_name is not None:
-            os.makedirs(self.model_name, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-          # Retrieve training reward
-            x, y = ts2xy(load_results(self.log_path), "timesteps")
-            if len(x) > 0:
-                # Mean training reward over the last cpu_num episodes
-                ep_len = len(y)
-                
-                mean_reward_ncpu = np.mean(y[-self.num_cpu:])
-                self.mean_reward_ncpu.append(mean_reward_ncpu)
-                self.rew_timesteps_ncpu.append(self.num_timesteps)
-                self.logger.record("rollout/mean_reward_ncpu", mean_reward_ncpu)
-                np.savez(self.model_name+'_ncpu',
-                         timesteps=self.rew_timesteps_ncpu,
-                         results=self.mean_reward_ncpu,
-                         ep_lengths=self.rew_length)
-                
-                if self.ep_len != ep_len:
-                    self.logger.record("rollout/mean_reward_step", np.mean(y[self.ep_len-ep_len:]))
-                    self.ep_len = ep_len
-                    self.logger.record("rollout/ep_len", ep_len)
-                    if self.ep_len // self.num_cpu != self.mean_ep_num:
-                        ep_shift = self.ep_len % self.num_cpu
-                        mean_reward = np.mean(y[-self.num_cpu-ep_shift:-1-ep_shift])
-                        self.logger.record("rollout/mean_reward", mean_reward)
-                        self.rew_results.append(mean_reward)
-                        self.rew_timesteps.append(self.num_timesteps)
-                        np.savez(self.model_name,
-                                 timesteps=self.rew_timesteps,
-                                 results=self.rew_results,
-                                 ep_lengths=self.rew_length)
-                        self.mean_ep_num = self.ep_len // self.num_cpu # += 1
-
-        return True
-
 def main(opt):
-   
     if opt.novel:
-        env = make_vec_env(env_id,
+        env = make_vec_env(ENV_ID,
                        n_envs = opt.num_cpu,
                        wrapper_class = wrapper,
                        monitor_dir = opt.log_prefix + f"logs/{opt.model_name}_monitor/")
     else:
-        env = make_vec_env(env_id,
+        env = make_vec_env(ENV_ID,
                n_envs = opt.num_cpu,
                monitor_dir = opt.log_prefix + f"logs/{opt.model_name}_monitor/")
     if opt.frame_stack_size != 0:
         env = VecFrameStack(env, opt.frame_stack_size)
-    
+
     callback_list = []
-    callback_list.append(SaveOnBestTrainingRewardCallback(128,
-                                                opt.num_cpu,
-                                                opt.log_prefix + f"logs/{opt.model_name}_monitor/",
-                                                opt.log_prefix + "results/" + opt.model_name + "/rewards"))
+
+    if opt.model_num_saves != 0:
+        callback_list.append(CheckpointCallback(
+            save_freq = opt.total_timesteps // opt.model_num_saves // opt.num_cpu,
+            save_path = opt.log_prefix + "results/" + opt.model_name,
+            name_prefix = opt.model_name,
+            save_replay_buffer = False,
+            save_vecnormalize = True,
+            ))
+
+    callback_list.append(SaveOnBestTrainingRewardCallback(
+        check_freq = 1024 // opt.num_cpu,
+        num_cpu = opt.num_cpu,
+        log_path = opt.log_prefix + f"logs/{opt.model_name}_monitor/",
+        model_save_path = opt.log_prefix + f"results/{opt.model_name}/",
+        model_name = opt.model_name
+    ))
+
     if opt.eval_freq != 0:
         callback_list.append(EvalCallback(env,
-                                 best_model_save_path= opt.log_prefix + "logs/best_model/" + opt.model_name,
-                                 # n_eval_episodes = 10,
-                                 log_path= opt.log_prefix + "results/" + opt.model_name,
-                                 eval_freq=opt.eval_freq,
-                                 deterministic=True, render=False))
+                            best_model_save_path= opt.log_prefix + "logs/best_model/" + opt.model_name,
+                            # n_eval_episodes = 1,
+                            log_path= opt.log_prefix + "results/" + opt.model_name,
+                            eval_freq=opt.eval_freq,
+                            deterministic=True,
+                            render=False))
     
     model = PPO("MlpPolicy",
             env,
@@ -156,7 +92,7 @@ def parse_opt(known=False):
     parser.add_argument('--eval-freq', type=int, default=0, help='eval freq')
     parser.add_argument('--num-cpu', type=int, default=32, help='Num cpu')
 
-    return parser.parse_known_args()[0] if known else parser.parse_args()    
+    return parser.parse_known_args()[0] if known else parser.parse_args()
     
 def run(**kwargs):
     # Usage: import train; train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt')
